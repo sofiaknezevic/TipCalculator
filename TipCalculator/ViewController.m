82e7fa017064dba334be2c20b62a57e07bc23155
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-17 11:37:08 -0800

    Make button calculate default tip

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (nonatomic, assign) CGFloat defaultTip;
+
 
 @end
 
@@ -17,7 +20,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.defaultTip = 0.15;
+    
+    
 }
+
+- (IBAction)calculateTip:(UIButton *)sender {
+    
+    double billAmount = [self.billAmountTextField.text floatValue];
+    
+    //NSLog(@"Your bill amount is, $%.02f", billAmount);
+    
+    double tipAmount = (billAmount * self.defaultTip);
+    
+    //NSLog(@"Your tip amount is $%0.2f", tipAmount);
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
