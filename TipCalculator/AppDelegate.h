802f2cc655299749853c026b3987d06a253f2153
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-17 08:33:13 -0800

    Initial commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Sofia Knezevic on 2017-02-17.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Sofia Knezevic on 2017-02-17.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
